d48281f82d083122d4ce656ecbbf0fceeceed68f
Diogo Ramos <64709603+diogonsramos@users.noreply.github.com>
2022-03-18 13:06:37 -0300

    Update ex02.py
    Add reading data and saving figure.

--- outras_aplicacoes_python/ex02.py
+++ outras_aplicacoes_python/ex02.py
@@ -1,7 +1,19 @@
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import cartopy.feature as cfeature
- 
+import xarray as xr 
+import matplotlib 
+
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 16}
+
+matplotlib.rc('font', **font)
+
+## Lendo o dataset criado no ex01.py (sempre verifique o nome do arquivo!)
+ds = xr.open_dataset('gfs.0p25.2018022000.f036.nc')
+
+fig = plt.figure(figsize=(12, 10))
 ax = plt.axes(projection=ccrs.PlateCarree())
  
 # Adiciona os contornos estaduais, com resolução de 50m, a partir da base NaturalEarth (https://www.naturalearthdata.com/).
@@ -13,4 +25,8 @@ states_provinces = cfeature.NaturalEarthFeature(
  
 ax.add_feature(states_provinces, edgecolor='k')
  
-ds['Wind_speed_gust_surface'].plot(ax=ax, cmap='bwr')
+## Fazendo o plot
+ds['Wind_speed_gust_surface'].plot(ax=ax, cmap='jet')
+
+## Salvando a figura
+fig.savefig('ex02.png', dpi=300, bbox_inches='tight')
